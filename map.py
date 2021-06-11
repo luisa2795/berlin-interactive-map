9f219c11e0931bb95443fd7bd0a39a33715f739f
gesara12 <s_halili20@stud.hwr-berlin.de>
2021-06-11 21:21:53 +0200

    changes

--- map.py
+++ map.py
@@ -3,12 +3,10 @@ import folium
 from streamlit_folium import folium_static
 
 #create Berlin Map
-m = folium.Map(location = [52.520008, 13.404954], zoom_start=10 )
+m = folium.Map(location = [52.520008, 13.404954], tiles = "cartodbpositron", zoom_start=10 ) 
 
-#show Alexander Platz
-# add marker for Liberty Bell
-tooltip = "Alexander Platz"
-folium.Marker([52.522150, 13.413140], popup="Liberty Bell", tooltip=tooltip ).add_to(m)
+# add marker for Alexander Platz
+folium.Marker([52.522150, 13.413140], popup="Alexander Platz", tooltip="Alexander Platz" ).add_to(m)
 
 #call the map
 folium_static(m)
