7ff8e5e295c47cb875e700ccc267394085a1072a
gesara12 <s_halili20@stud.hwr-berlin.de>
2021-07-08 21:32:41 +0200

    cache added

--- app.py
+++ app.py
@@ -10,8 +10,30 @@ import pymongo
 from shapely.geometry import Point, shape
 import dns
 import base64
-from functions import load_data
 from folium.plugins import Fullscreen
+import pymongo
+import requests
+import json
+
+@st.cache(allow_output_mutation=True, show_spinner=False)
+def load_data():
+    #initialize mongo client and database
+    client = pymongo.MongoClient('mongodb+srv://doadmin:A79tz5F16P3Z84kW@berlin-map-db-d1b8496c.mongo.ondigitalocean.com/berlin-map?authSource=admin&replicaSet=berlin-map-db&tls=true&tlsCAFile=ca-certificate.crt')
+    db = client['berlin-map']
+    #Load Distric data
+    districts=list(db.districtcoordinates.find())
+    #Load WC data
+    toilets=list(db.toilets.find())
+    #Load Swimming Spots data
+    response=requests.get("https://www.berlin.de/lageso/gesundheit/gesundheitsschutz/badegewaesser/liste-der-badestellen/index.php/index/all.gjson?q=")
+    swim_spots=json.loads(response.text)['features']
+    #Load Memorials data
+    items = list(db.memorials.find())
+    #Load Monuments data
+    items2 = list(db.monuments.find())
+    client.close()
+    return toilets, response, swim_spots, districts, items, items2
+
 
 toilets, response, swim_spots, districts, items, items2 = load_data()
 
